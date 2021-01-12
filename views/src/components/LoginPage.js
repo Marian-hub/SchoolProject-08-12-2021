d2043d339c31e7c8ce6fbef17708330a3b0673c7
marian <marian@localhost.localdomain>
2021-01-12 22:26:47 +0100

    adding documentation

---
+++ views/src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import axios from 'axios'
+class LoginPage extends Component{
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default LoginPage
